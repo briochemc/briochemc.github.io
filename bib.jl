1d573a13fd6aed53b688113e10aab06442fe09f7
Benoit Pasquier <briochemc@gmail.com>
2024-07-30 13:53:44 +1000

    Add doi to PhD

--- bib.jl
+++ bib.jl
@@ -373,9 +373,10 @@ function format_bibtex_entry(entry, key; list_style="number")
     """
     # pubdetails = """$(formatlazyspan(entry, "editor"; prefix="in: "))$(formatlazyspan(entry,"booktitle"; prefix= haskey(entry,"editor") ? ": " : "in: "))$(formatlazyspan(entry, "chapter"; prefix=", Chapter "))$(formatlazyspan(entry,"journaltitle";class="journal"))$(formatlazyspan(entry,"series"; prefix=", "))$(formatlazyspan(entry,"volume"))$(formatlazyspan(entry,"number"))$(formatlazyspan(entry,"issue"))$(formatlazyspan(entry,"pages", prefix=pagesprefix(entry)))$(formatlazyspan(entry,"publisher"; prefix=", "))$(formatlazyspan(entry,"type"))$(formatlazyspan(entry,"language"; prefix=", "))$(formatlazyspan(entry,"school"; prefix=", "))$(formatlazyspan(entry,"note"))$(formatspan(entry,"year"))"""
     # pubdetails = """$(formatlazyspan(entry, "editor"; prefix="in: "))$(formatlazyspan(entry,"booktitle"; prefix= haskey(entry,"editor") ? ": " : "in: "))$(formatlazyspan(entry, "chapter"; prefix=", Chapter "))$(formatlazyspan(entry,"journaltitle";class="journal"))$(formatspan(entry,"year"))"""
-    pubdetails = """$(formatlazyspan(entry,"journaltitle";class="journal"))$(formatlazyspan(entry,"year"))"""
     title = formatspan(entry,"title"; remove=["{","}"])
     title = latex2html(title)
+    pubdetails = """$(formatlazyspan(entry,"school"))$(formatlazyspan(entry,"journaltitle";class="journal"))"""
+    year = formatlazyspan(entry,"year")
     s = """$s
            $(title)
         """
@@ -384,6 +385,9 @@ function format_bibtex_entry(entry, key; list_style="number")
         """
     s = """$s
            $(pubdetails)
+        """
+    s = """$s
+           $(year)
         """
     # s = """$s
     #        $(entry_to_list_icon(entry,"pdf"; iconstyle="fas fa-md", icon="fa-download"))
